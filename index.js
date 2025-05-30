b6a8a94583cb92e1bf1597c2dce4ef4ebe2793bd
Fabricio <fabriciozulian@outlook.com>
2025-05-30 13:14:42 -0300

    Adicionando funcionalidade no menu

---
+++ index.js
@@ -0,0 +1,6 @@
+const botaoMenu = document.querySelector(".cabecalho__menu")
+
+botaoMenu.addEventListener("click", (e) => {
+    document.querySelector(".menu-lateral").classList.toggle('menu-lateral--ativo');
+    alert("teste");
+});
