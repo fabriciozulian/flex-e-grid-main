bdc8352d569bfe1ac2fe24596eb0788915e19935
Fabricio <fabriciozulian@outlook.com>
2025-06-20 13:31:39 -0300

    feat: incluindo card principal

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const botaoMenu = document.querySelector(".cabecalho__menu")
 
 botaoMenu.addEventListener("click", (e) => {
     document.querySelector(".menu-lateral").classList.toggle('menu-lateral--ativo');
-    alert("teste");
 });
